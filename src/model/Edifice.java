c390826fb1a0558a490f24e1df46375938f13f16
daniel--jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2022-02-18 19:48:22 +0100

    REFACTOR enum Buildigs and there method makeEdifice

--- src/model/Edifice.java
+++ src/model/Edifice.java
@@ -5,11 +5,11 @@ import java.util.Set;
 
 
 public class Edifice {
-    private Buildings.name name;
+    private Buildings name;
     private int minHeat;
     private int currentHeat;
     private Set<Person> people;
-    protected Edifice(Buildings.name name, int currentHeat, int minHeat){
+    protected Edifice(Buildings name, int currentHeat, int minHeat){
         this.name = name;
         this.currentHeat = currentHeat;
         this.minHeat = minHeat;
