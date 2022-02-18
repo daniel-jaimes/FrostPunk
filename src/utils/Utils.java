3532fc8589eb055af973ef975c7122352a01436d
daniel--jaimes <48568101+daniel-jaimes@users.noreply.github.com>
2022-02-18 20:04:19 +0100

    encapsulation

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -13,40 +13,59 @@ public class Utils {
     public static Actions returnWhichActionIs(String line) throws LogicException {
 
         String[] elementsLine = line.trim().split(" ");
-
-        if (elementsLine.length == 1 ){
-            //ACTION "S" - PRINT PERSON IN REFUGES
-            if (elementsLine[0].equalsIgnoreCase("S")){
-                return Actions.PRINT_REFUGE_PERSONS;
-            }
-            //ACTION "C" - PRINT ALL INFORMATION OF CITY
-            if (elementsLine[0].equalsIgnoreCase("C")){
-                return Actions.PRINT_CITY_INFO;
-            }
+        //ACTION "S" - PRINT PERSON IN REFUGES
+        if (checkSyntaxActionPrintPersonsInRefuges(elementsLine)) {
+            return Actions.PRINT_REFUGE_PERSONS;
         }
-        if (elementsLine.length == 2){
-            //ACCION "T valor" - MODIFY TEMPERATURE
-            if (elementsLine[0].length() == 1 && isDigit(elementsLine[1])){
-                return Actions.MODIFY_TEMPERATURE;
-            }
-            //ACCION "<circle, distrito> NombreEdificio" - ADD EDIFICE
-            if (isCordenate(elementsLine[0]) && isCorrectName(elementsLine[1])){
-                return Actions.ADD_EDIFICE;
-            }
+        //ACTION "C" - PRINT ALL INFORMATION OF CITY
+        if(checkSyntaxActionPrintInfoCity(elementsLine)){
+            return Actions.PRINT_CITY_INFO;
         }
-        if (elementsLine.length == 3){
-            //ACCION "+ NombrePersona <circle, distrito>" - ADD PERSON TO REFUGE
-            if (elementsLine[0].length() == 1 && elementsLine[0].equals("+") && isCordenate(elementsLine[2])){
-                return Actions.ADD_PERSON_TO_REFUGE;
-            }
-            //ACCION " > <circleRefugio, distritoRefugio> <circleDestino, distritoDestino>" - ADD JOB ONE PERSONE
-            if (elementsLine[0].length() == 1 && elementsLine[0].equals("+")
-                    && isCordenate(elementsLine[1]) && isCordenate(elementsLine[2])){
-                return Actions.ADD_JOB_PERSON;
-            }
+        //ACCION "T valor" - MODIFY TEMPERATURE
+        if(checkSyntaxActionModifyTemperature(elementsLine)){
+            return Actions.MODIFY_TEMPERATURE;
+        }
+        //ACCION "<circle, distrito> NombreEdificio" - ADD EDIFICE
+        if(checkSyntaxActionAddEdifice(elementsLine)){
+            return Actions.ADD_EDIFICE;
+        }
+        //ACCION "+ NombrePersona <circle, distrito>" - ADD PERSON TO REFUGE
+        if(checkSyntaxActionAddPersonaToRefuge(elementsLine)){
+            return Actions.ADD_PERSON_TO_REFUGE;
+        }
+        //ACCION " > <circleRefugio, distritoRefugio> <circleDestino, distritoDestino>" - ADD JOB ONE PERSONE
+        if (checkSyntaxActionAddJobPerson(elementsLine)) {
+            return Actions.ADD_JOB_PERSON;
         }
 
         throw new LogicException(LogicException.INCORRECT_ACTION);
+    }
+    //ACTION SYNTAX CHECKERS
+    private static boolean checkSyntaxActionPrintInfoCity(String[] elementsLine) {
+        return elementsLine.length == 1 && elementsLine[0].equalsIgnoreCase("C");
+    }
+
+    private static boolean checkSyntaxActionPrintPersonsInRefuges(String[] elementsLine) {
+        return elementsLine.length == 1 && elementsLine[0].equalsIgnoreCase("S");
+    }
+
+    private static boolean checkSyntaxActionAddJobPerson(String[] elementsLine) {
+        return elementsLine.length == 3 && elementsLine[0].length() == 1
+                && elementsLine[0].equals("+") && isCordenate(elementsLine[1])
+                && isCordenate(elementsLine[2]);
+    }
+
+    private static boolean checkSyntaxActionAddPersonaToRefuge(String[] elementsLine) {
+        return elementsLine.length == 3 && elementsLine[0].length() == 1
+                && elementsLine[0].equals("+") && isCordenate(elementsLine[2]);
+    }
+
+    private static boolean checkSyntaxActionAddEdifice(String[] elementsLine) {
+        return elementsLine.length == 2 && isCordenate(elementsLine[0]) && isCorrectName(elementsLine[1]);
+    }
+
+    private static boolean checkSyntaxActionModifyTemperature(String[] elementsLine) {
+        return elementsLine.length == 2 && elementsLine[0].length() == 1 && isDigit(elementsLine[1]);
     }
 
     private static boolean isDigit( String number){
@@ -67,4 +86,5 @@ public class Utils {
     private static boolean isCorrectName(String nameEdifice){
         return Arrays.asList(Buildings.values()).contains(Buildings.valueOf(nameEdifice));
     }
+
 }
