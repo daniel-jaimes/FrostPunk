f22793f85b8c63d08ccfa37d7cba101ca03467a7
Daniel Jaimes <dajaca00@gmail.com>
2022-02-21 15:32:43 +0100

    add two messages exceptions

--- src/exceptions/LogicException.java
+++ src/exceptions/LogicException.java
@@ -6,10 +6,14 @@ import java.util.List;
 public class LogicException extends Exception{
     public static final int INCORRECT_ACTION = 0;
     public static final int INCORRECT_NAME_EDIFICE = 1;
+    public static final int EDIFICE_CORRECTED_ADDED = 2;
+    public static final int PLOT_IS_NOT_EMPTY = 3;
     private int value;
     private List<String> message = Arrays.asList(
             "<< La accion es incorrecta. >>",
-            "<< El nombre del edificio no existe >>"
+            "<< El nombre del edificio no existe >>",
+            "<< Edificio added >>",
+            "<< La parcela no está vacia >>"
     );
     public LogicException(int value){
         this.value = value;
