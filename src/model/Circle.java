8a443df87186796ba8d556503ce3c74d72deb0fe
Daniel Jaimes <dajaca00@gmail.com>
2022-02-21 15:37:29 +0100

    - Array to ArrayList - add geter element of District arraylist.

--- src/model/Circle.java
+++ src/model/Circle.java
@@ -1,12 +1,25 @@
 package model;
 
-import java.util.TreeSet;
+import java.util.ArrayList;
 
 public class Circle {
-    private int id;
-    private TreeSet<District> district;
-    protected Circle(int id){
-        this.id = id;
-        this.district = null;
+    private ArrayList<District> districts;
+    protected Circle(){
+        this.districts = new ArrayList<District>(){{
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+            add(new District());
+        }};
     }
+    public District getDistrict(int id){
+        return districts.get(id - 1);
+    }
+
 }
