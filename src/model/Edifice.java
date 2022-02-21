22895838a09471add48673120377a149cd7ca22e
Daniel Jaimes <dajaca00@gmail.com>
2022-02-21 11:52:02 +0100

    change name to more semantic name

--- src/model/Edifice.java
+++ src/model/Edifice.java
@@ -5,15 +5,30 @@ import java.util.Set;
 
 
 public class Edifice {
-    private Buildings name;
+    private String name;
     private int minHeat;
     private int currentHeat;
     private Set<Person> people;
-    protected Edifice(Buildings name, int currentHeat, int minHeat){
+    protected Edifice(String name, int currentHeat, int minHeat){
         this.name = name;
         this.currentHeat = currentHeat;
         this.minHeat = minHeat;
         people = new HashSet<>();
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getMinHeat() {
+        return minHeat;
+    }
+
+    public int getCurrentHeat() {
+        return currentHeat;
+    }
+
+    public Set<Person> getPeople() {
+        return people;
+    }
 }
