7c8d348a41085157058f85b1918d15dc08e2d85a
Daniel Jaimes <dajaca00@gmail.com>
2022-02-21 15:33:12 +0100

    implement compareTo

--- src/model/Person.java
+++ src/model/Person.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Person {
+public class Person implements Comparable<Person>{
     private String name;
     private Integer idCircle;
     private Integer idDistrict;
@@ -14,5 +14,10 @@ public class Person {
                 ", circle=" + idCircle +
                 ", distrito=" + idDistrict +
                 ']';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.name.compareTo(o.name);
     }
 }
