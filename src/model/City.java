a9b10c8abe7a4d85711d420f9dad02ecffe511c1
Daniel Jaimes <dajaca00@gmail.com>
2022-02-21 15:36:58 +0100

    - Array to ArrayList - add geter element of Circle arraylist.

--- src/model/City.java
+++ src/model/City.java
@@ -5,19 +5,24 @@ import java.util.*;
 public class City {
     private List<Person> people;
     private int temperature;
-    private Circle[] circles;
+    private ArrayList<Circle> circles;
     public City(){
         temperature = 0;
-        circles = new Circle[]{
-                new Circle(1),
-                new Circle(2),
-
-        };
+        circles = new ArrayList<Circle>(){{
+            add(new Circle());
+            add(new Circle());
+            add(new Circle());
+            add(new Circle());
+            add(new Circle());
+        }};
     }
     public void addPerson(Person person){
         people.add(person);
     }
     public void modifyTemperature(int temperatureModifaction){
         this.temperature = this.temperature + temperatureModifaction;
+    }
+    public Circle getCircle(int id){
+        return circles.get(id);
     }
 }
